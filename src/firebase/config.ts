5f980c62a85f588c7f3aea4a2e11375dc0851cb0
Geovani silva de oliveira <geovanisilvadeoliveira447@gmail.com>
2025-12-17 00:46:55 +0000

    Move Firebase config to environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3511014535-25a3b",
-  "appId": "1:423554265650:web:fd8a4a4e0c036bd97e422b",
-  "apiKey": "AIzaSyBVv5zvyhi03spMBeCL2ciXzT5GjC0nwjk",
-  "authDomain": "studio-3511014535-25a3b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "423554265650"
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  "measurementId": process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
